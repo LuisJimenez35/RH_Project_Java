2518703dcbfd44f4978cc83343f06e74c8b7e6ad
Luis Mora <82769778+LuisJimenez35@users.noreply.github.com>
2023-08-02 17:19:34 -0600

    Update 2/8/23

--- RH_Project_Java/src/Welcome_Functions/Main_Window.java
+++ RH_Project_Java/src/Welcome_Functions/Main_Window.java
@@ -27,13 +27,13 @@ public class Main_Window extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        PassInput = new javax.swing.JPasswordField();
         jButton3 = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         LoginButton = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        PassInput = new javax.swing.JTextField();
         UserInput = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -41,10 +41,18 @@ public class Main_Window extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        PassInput.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 14)); // NOI18N
+        getContentPane().add(PassInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 220, 160, 40));
+
         jButton3.setBackground(new java.awt.Color(102, 0, 0));
         jButton3.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 18)); // NOI18N
         jButton3.setForeground(new java.awt.Color(255, 255, 255));
         jButton3.setText("Close System");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 390, 180, 40));
 
         jLabel5.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 36)); // NOI18N
@@ -79,14 +87,6 @@ public class Main_Window extends javax.swing.JFrame {
         jLabel4.setText("USER");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 90, -1, -1));
 
-        PassInput.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 14)); // NOI18N
-        PassInput.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                PassInputActionPerformed(evt);
-            }
-        });
-        getContentPane().add(PassInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 220, 160, 40));
-
         UserInput.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 14)); // NOI18N
         getContentPane().add(UserInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 130, 160, 40));
 
@@ -100,10 +100,6 @@ public class Main_Window extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void PassInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PassInputActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_PassInputActionPerformed
 
     private void LoginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoginButtonActionPerformed
         String User = UserInput.getText();
@@ -135,6 +131,10 @@ public class Main_Window extends javax.swing.JFrame {
             e.printStackTrace();
         }
     }//GEN-LAST:event_LoginButtonActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,7 +173,7 @@ public class Main_Window extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton LoginButton;
-    private javax.swing.JTextField PassInput;
+    private javax.swing.JPasswordField PassInput;
     private javax.swing.JTextField UserInput;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
